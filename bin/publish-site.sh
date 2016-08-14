280283b0d124e6c5f6334fd57205249953a57215
Nolan Lawson <nolan.lawson@gmail.com>
2016-08-14 16:42:50 -0700

    lighter gh-pages page

--- bin/publish-site.sh
+++ bin/publish-site.sh
@@ -3,7 +3,7 @@
 npm run build
 BRANCH_NAME=build_"$RANDOM"
 git checkout -b $BRANCH_NAME
-git add -f dist lib script.es5.js
+git add -f dist/*.min.js script.es5.js
 git commit -m 'build'
 git push --force origin $BRANCH_NAME:gh-pages
 git checkout master
